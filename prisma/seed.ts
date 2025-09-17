2b0adfd9be055fb003c1e4754f149c26db0a6000
edenWerke <edenw6292@gmail.com>
2025-09-17 20:02:51 +0300

    seed data

---
+++ prisma/seed.ts
@@ -0,0 +1,77 @@
+import { PrismaClient, Prisma } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const userData: Prisma.UserCreateInput[] = [
+  {
+    name: "jhon",
+    email: "jhon@prisma.com",
+    post: {
+      create: [
+        {
+          title: "Join the Prisma Slack",
+          published: true,
+          catagories: {
+            create: [
+              { name: "furniture" },
+              { name: "fashion" },
+            ],
+          },
+        },
+      ],
+    },
+  },
+  {
+    name: "jack",
+    email: "jack@prisma.io",
+    post: {
+      create: [
+        {
+          title: "Follow Prisma",
+          published: true,
+          catagories: {
+            connect: [{ id: 1 }], // Connect to existing category ID
+          },
+        },
+      ],
+    },
+  },
+  {
+    name: "emma",
+    email: "emma@prisma.com",
+    post: {
+      create: [
+        {
+          title: "Learn Prisma ORM",
+          published: false,
+          catagories: {
+            connect: [{ id: 2 }], // Connect to existing category ID
+          },
+        },
+      ],
+    },
+  },
+];
+
+async function main() {
+  console.log("Start seeding");
+
+  for (const u of userData) {
+    const user = await prisma.user.create({
+      data: u,
+    });
+    console.log(`Created user with id: ${user.id}`);
+  }
+
+  console.log("Seeding finished");
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
