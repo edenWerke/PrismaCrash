4384c18a4458048ab28d5452b93b2d6c41cd9418
edenWerke <edenw6292@gmail.com>
2025-09-18 11:27:35 +0300

    operations

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -3,7 +3,9 @@ import prisma from "@/lib/prisma"
 export async function GET(){
     const users=await prisma.user.findMany({
         where:{
-            name:"jhon"
+            name:{
+                startsWith:"j"
+            }
         }
     })
     return new Response(JSON.stringify(users))
