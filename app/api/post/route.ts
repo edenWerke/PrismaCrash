91917ceb3f213cd08240fba6ed0cc7d7babc38ea
edenWerke <edenw6292@gmail.com>
2025-09-18 11:40:04 +0300

    adding the operaton in the post side

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -1,27 +1,22 @@
-// import prisma from "@/lib/prisma";
+import prisma from "@/lib/prisma"
 
-// export async function GET(){
-//     const posts=await prisma.post.findMany()
-//     return new Response (JSON.stringify(posts))
-// }
-import prisma from "@/lib/prisma";
-
-export async function GET() {
-  try {
-    const posts = await prisma.post.findFirst();
-    return new Response(JSON.stringify(posts), {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  } catch (error) {
-    console.error(error);
-    return new Response(JSON.stringify({ error: "Failed to fetch posts" }), {
-      status: 500,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  }
+export async function GET(){
+    const posts=await prisma.post.findMany({
+        where:{
+      
+            OR:[
+              {
+                title:{
+                  contains:"Learn"
+                }
+              },
+              {
+                title:{
+                  contains:"Join"
+                }
+              }
+            ]
+        }
+    })
+    return new Response(JSON.stringify(posts))
 }
--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -4,7 +4,7 @@ export async function GET(){
     const users=await prisma.user.findMany({
         where:{
             id:{
-                in:[1,3]
+                notIn:[1,3]
             }
         }
     })
