b725523307309350963bdc17375d880b94c5f50b
edenWerke <edenw6292@gmail.com>
2025-09-18 20:45:28 +0300

    avg

--- app/api/post/aggregation/route.ts
+++ app/api/post/aggregation/route.ts
@@ -4,6 +4,9 @@ export async function GET(){
     const aggregations =await prisma.post.aggregate({
         _sum:{
             likeNum:true
+        },
+        _avg:{
+likeNum:true
         }
     })
     return new Response(JSON.stringify(aggregations))
